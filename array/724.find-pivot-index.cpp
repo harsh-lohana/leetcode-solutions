3660f0888c8c65fbab8e32a1bf67ed24057221e5
Harsh Lohana <harshylohana15@gmail.com>
2023-01-27 17:56:49 +0530

    updated solutions

---
+++ array/724.find-pivot-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int i,sum = 0,left = 0,right = 0;
+        for(i = 0;i < nums.size();i++)
+            sum += nums[i];
+        for(i = 0;i < nums.size();i++)
+        {
+            right = sum - nums[i] - left;
+            if(left == right)
+                return i;
+            left += nums[i];
+        }
+        return -1;
+    }
+};
